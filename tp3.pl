4d6b8be2facf84aae4d35d27fc40985eeb824c24
Ricardo <no_email@no_server.org>
2015-05-09 16:30:27 +0100

    Add DCG grammar for Ex2 in prolog

--- tp3.pl
+++ tp3.pl
@@ -1,4 +1,5 @@
 
+% Exercise 1.
 /* static data */
 timetable(edinburgh,london,
 		[ 9:40/10:50/ba4733/alldays,
@@ -93,6 +94,111 @@ deptime([_-_:_:DepTime|_], DepTime).
 
 /* transfer(Time1, Time2) */
 /* in a nutshell, Time1 + 40 <= Time2 */
-/* <= apparently does not exist in Prolog */
+/* '<=' apparently does not exist in Prolog */
 transfer(H1:T1, H2:T2) :- N1 is ((H1 * 60 + T1) + 40) mod (24 * 60), N2 is (H2 * 60 + T2) + 1, N1 < N2.
+
+
+% Exercise 2.
+sentenca --> frase_nom, frase_verb.
+sentenca --> frase_nom_p, frase_verb_p.
+
+frase_nom --> artigo_f, subst_f.
+frase_nom --> artigo_m, subst_m.
+frase_nom --> subst_f.
+frase_nom --> subst_m.
+
+frase_nom_p --> artigo_p_f, subst_p_f.
+frase_nom_p --> artigo_p_m, subst_p_m.
+frase_nom_p --> subst_p_f.
+frase_nom_p --> subst_p_m.
+
+frase_verb --> verbo.
+frase_verb --> verbo, preposicao, frase_nom.
+frase_verb --> verbo, preposicao, frase_nom_p.
+frase_verb_p --> verbo_p.
+frase_verb_p --> verbo_p, preposicao, frase_nom.
+frase_verb_p --> verbo_p, preposicao, frase_nom_p.
+
+artigo_f --> [a].
+artigo_f --> ['A'].
+artigo_m --> [o].
+artigo_m --> ['O'].
+artigo_p_f --> [as].
+artigo_p_f --> ['As'].
+artigo_p_m --> [os].
+artigo_p_m --> ['Os'].
+
+subst_f --> [menina].
+subst_f --> [floresta].
+subst_f --> [mae].
+subst_f --> [vida].
+subst_f --> [noticia].
+subst_f --> [cidade].
+subst_f --> [porta].
+subst_p_f --> [lagrimas].
+subst_m --> [tempo].
+subst_m --> [cacador].
+subst_m --> [rio].
+subst_m --> [rosto].
+subst_m --> [mar].
+subst_m --> [vento].
+subst_m --> [martelo].
+subst_m --> [cachorro].
+subst_m --> [sino].
+subst_p_m --> [tambor].
+subst_p_m --> [lobos].
+subst_p_m --> [tambores].
+
+verbo --> [corre].
+verbo --> [correu].
+verbo --> [bateu].
+verbo_p --> [correram].
+verbo_p --> [corriam].
+verbo_p --> [batiam].
+verbo_p --> [bateram].
+
+% the test cases don't seem to need f/m or s/p qualifiers
+preposicao --> [para].
+preposicao --> [pela]. % s f
+preposicao --> [com].
+preposicao --> [pelo]. % s m
+preposicao --> [a]. % s f
+preposicao --> [no]. % s m
+preposicao --> [na]. % s f
+
+/* frases corretas */
+/*
+sentenca(['A',menina,corre,para,a,floresta],[]).
+sentenca(['A',menina,corre,para,a,mae],[]).
+sentenca(['A',vida,corre],[]).
+sentenca(['O',tempo,corre],[]).
+sentenca(['O',cacador,correu,com,os,lobos],[]).
+sentenca(['A',noticia,correu,pela,cidade],[]).
+sentenca(['As',lagrimas,corriam,pelo,rosto],[]).
+sentenca(['O',rio,corre,para,o,mar],[]).
+sentenca(['A',menina,bateu,a,porta],[]).
+sentenca(['A',porta,bateu],[]).
+sentenca(['O',vento,bateu,a,porta],[]).
+sentenca(['A',menina,bateu,na,porta],[]).
+sentenca(['O',martelo,bateu,na,porta],[]).
+sentenca(['A',menina,bateu,no,cachorro],[]).
+sentenca(['A',menina,bateu,no,tambor],[]).
+sentenca(['Os',tambores,bateram],[]).
+sentenca(['O',sino,bateu],[]).
+sentenca(['A',menina,corre],[]).
+sentenca(['A',vida,correu],[]).
+sentenca(['A',noticia,correu,para,a,floresta],[]).
+sentenca(['A',vida,correu,com,os,lobos],[]).
+sentenca(['A',menina,bateu,a,mae],[]).
+*/
+
+/* frases erradas */
+/*
+sentenca(['A',tempo,corre],[]).
+sentenca(['O',tempo,correram],[]).
+sentenca(['A',cacador,corriam,pela,rosto],[]).
+sentenca(['A',tambores,correu,pela,floresta],[]).
+sentenca(['Os',tambores,bateu,na,porta],[]).
+sentenca(['O',sino,bateu,na,meninas],[]).
+*/
 
